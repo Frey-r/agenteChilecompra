a46827073f17b3556dbccdb6c82e125c6ac850ca
Eduardo Bachmann <eduardo.bachmann@idok.cl>
2025-08-25 00:54:19 -0400

    feat: add FastAPI server with basic root endpoint

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import util.logger_config as logger_config
+from fastapi import FastAPI
+from uvicorn import run
 
+app = FastAPI()
 
 logger = logger_config.get_logger(__name__)
 logger.info("Inicializando agente")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    run(app, host="0.0.0.0", port=8000)
 
